f2fad7d7b70276206838786b3211d15a4fe164ac
Julio César Lara Lerma <s21120175@alumnos.itsur.edu.mx>
2025-05-20 00:50:51 -0600

    modelo de personaje actualizado

--- composeApp/src/commonMain/kotlin/org/example/project/data/RivalsCharacter.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/data/RivalsCharacter.kt
@@ -1,6 +1,5 @@
 package org.example.project.data
 
-
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
@@ -16,7 +15,9 @@ data class RivalsCharacter(
     val difficulty: String? = null,
     val bio: String? = null,
     val lore: String? = null,
-    val transformations: List<Transformation>? = null
+    val transformations: List<Transformation>? = null,
+    val costumes: List<Costume>? = null,
+    val abilities: List<Ability>? = null
 ) {
     @Serializable
     data class Transformation(
@@ -25,6 +26,28 @@ data class RivalsCharacter(
         val icon: String? = null,
         val health: String? = null,
         @SerialName("movement_speed") val movementSpeed: String? = null
+    )
+
+    @Serializable
+    data class Costume(
+        val id: String,
+        val name: String,
+        val icon: String? = null,
+        val quality: String? = null,
+        val description: String? = null,
+        val appearance: String? = null
+    )
+
+    @Serializable
+    data class Ability(
+        val id: Int,
+        val icon: String? = null,
+        val name: String? = null,
+        val type: String? = null,
+        val isCollab: Boolean = false,
+        val description: String? = null,
+        @SerialName("additional_fields") val additionalFields: Map<String, String>? = null,
+        @SerialName("transformation_id") val transformationId: String? = null
     )
 }
 
